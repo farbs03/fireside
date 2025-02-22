a928acbf50ed2469fc3818f9a0cb63cc097557b7
farbs03 <cgeraldfarber@icloud.com>
2025-02-22 15:18:21 -0500

    layout for fireside dashboard

--- src/app/firesides/components/Map.tsx
+++ src/app/firesides/components/Map.tsx
@@ -18,7 +18,7 @@ interface MarkerData {
 export default function Map() {
   const defaultPosition: [number, number] = [34.0549, -118.2451];
   const [marker, setMarker] = useState<MarkerData | null>(null);
-
+  
   useEffect(() => {
     return () => {
       const containers = document.querySelectorAll(".leaflet-container");
@@ -31,14 +31,14 @@ export default function Map() {
   }, []);
 
   return (
-    <div>
-      <Navbar />
+    <div className="flex min-h-screen flex-col">
       <MapContainer
         id="map"
-        center={defaultPosition}
+        center={!marker ? defaultPosition : marker.position}
         zoom={11}
         scrollWheelZoom={true}
-        style={{ height: "100vh", width: "100%" }}
+        className="h-full flex-grow"
+        zoomControl={false}
       >
         <TileLayer
           attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
@@ -46,18 +46,24 @@ export default function Map() {
         />
         {/* Render marker if an address has been searched */}
         {marker && (
-          <Marker icon={MapPinMarker} position={marker.position}>
+          <Marker
+            icon={MapPinMarker}
+            position={marker.position}
+            draggable={true}
+          >
             <Popup>{marker.displayName}</Popup>
           </Marker>
         )}
         {/* Optionally include the basic geocoder control */}
         <GeocoderControl />
         {/* Include the new autocomplete search and pass the onSelect callback */}
-        <AddressSearch
-          onSelect={(lat, lon, displayName) => {
-            setMarker({ position: [lat, lon], displayName });
-          }}
-        />
+        <div className="">
+          <AddressSearch
+            onSelect={(lat, lon, displayName) => {
+              setMarker({ position: [lat, lon], displayName });
+            }}
+          />
+        </div>
       </MapContainer>
     </div>
   );
--- src/app/firesides/page.tsx
+++ src/app/firesides/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import Navbar from "~/components/navbar";
+import Dashboard from "./components/Dashboard";
 
 const LazyMap = dynamic(() => import("./components/Map"), {
   ssr: false,
@@ -9,8 +11,16 @@ const LazyMap = dynamic(() => import("./components/Map"), {
 
 export default function Home() {
   return (
-    <main>
-      <LazyMap />
+    <main className="flex min-h-screen flex-col">
+      <Navbar />
+      <div className="flex h-full w-full flex-grow" mx-auto>
+        <div className="w-full flex-grow">
+          <LazyMap />
+        </div>
+        <div className="w-full max-w-[300px] flex-grow">
+          <Dashboard />
+        </div>
+      </div>
     </main>
   );
 }
