8d213c9d27dd8b68c0e02a5517f02f420c7de650
farbs03 <cgeraldfarber@icloud.com>
2025-02-22 23:18:09 -0500

    changes to schema

--- prisma/migrations/20250223040453_reset_pls/migration.sql
+++ prisma/migrations/20250223040453_reset_pls/migration.sql
@@ -1,15 +1,4 @@
 -- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "createdById" TEXT NOT NULL,
-
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
@@ -45,6 +34,7 @@ CREATE TABLE "User" (
     "email" TEXT,
     "emailVerified" TIMESTAMP(3),
     "image" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'user',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -57,19 +47,19 @@ CREATE TABLE "VerificationToken" (
 );
 
 -- CreateTable
-CREATE TABLE "Location" (
+CREATE TABLE "Fireside" (
     "id" SERIAL NOT NULL,
     "lat" DOUBLE PRECISION NOT NULL,
     "lng" DOUBLE PRECISION NOT NULL,
-    "userId" TEXT,
+    "displayName" TEXT NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "volunteerIds" TEXT[],
+    "availableResources" TEXT[],
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Fireside_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE INDEX "Post_name_idx" ON "Post"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
@@ -85,9 +75,6 @@ CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token"
 
 -- CreateIndex
 CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
-
--- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/server/api/routers/fireside.ts
+++ src/server/api/routers/fireside.ts
@@ -7,17 +7,18 @@ import {
 } from "~/server/api/trpc";
 
 export const firesideRouter = createTRPCRouter({
-  getAll: protectedProcedure
-    .query(async ({ctx}) => {
-      return ctx.db.fireside.findMany()
-    }),
+  getAll: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.db.fireside.findMany();
+  }),
   create: protectedProcedure
-    .input(z.object({ 
-      displayName: z.string().min(1), 
-      lng: z.number(), 
-      lat: z.number(), 
-      creatorId: z.string() 
-    }))
+    .input(
+      z.object({
+        displayName: z.string().min(1),
+        lng: z.number(),
+        lat: z.number(),
+        creatorId: z.string(),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
       return ctx.db.fireside.create({
         data: {
